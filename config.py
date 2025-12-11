672b47bc2a278f2758c5ff11520b75097dcb46f9
chuhan <chuhanchik3131@gmail.com>
2025-12-11 22:52:59 +0500

    chore: add PUBLIC_KEY to config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
 KEYS_COUNTER = os.getenv("KEYS_COUNTER") or "keys_counter.txt"
+PUBLIC_KEY = "b7a92b4cd1a2ced29e06059c61f624be" # no need to change
 
 required_vars = ["BOT_TOKEN"]
 for var in required_vars:
