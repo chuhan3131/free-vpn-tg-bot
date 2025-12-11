22c009eb7836573139336710b864d102da3e0fff
chuhan <chuhanchik3131@gmail.com>
2025-12-11 21:53:00 +0500

    chore: add KEYS_COUNTER environment variable

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
+KEYS_COUNTER = os.getenv("KEYS_COUNTER") or "keys_counter.txt"
 
 required_vars = ["BOT_TOKEN"]
 for var in required_vars:
