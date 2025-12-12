72efa60c047011245ff2b9a684d5966181308b70
chuhan <chuhanchik3131@gmail.com>
2025-12-12 20:23:43 +0500

    feat: add inline query support to main bot

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
-from utils.handlers import setup_handlers
+from handlers.common import common_handlers
+from handlers.inline import router as inline_router
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
 async def main():
-    setup_handlers(dp)
+    common_handlers(dp)
+    dp.include_router(inline_router)
+    
     await dp.start_polling(bot)
 
 
