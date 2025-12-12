e1b76f79f6960a453e95db0428274c660f861819
chuhan <chuhanchik3131@gmail.com>
2025-12-12 07:39:45 +0500

    chore: add PUBLIC_KEY to required environment variables

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
 KEYS_COUNTER = os.getenv("KEYS_COUNTER") or "keys_counter.txt"
 PUBLIC_KEY = "b7a92b4cd1a2ced29e06059c61f624be" # no need to change
 
-required_vars = ["BOT_TOKEN"]
+required_vars = ["BOT_TOKEN", "PUBLIC_KEY"]
 for var in required_vars:
     if not globals()[var]:
         raise ValueError(
