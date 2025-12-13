012bc64a679365925a0fea010abaa73105dd30de
chuhan <chuhanchik3131@gmail.com>
2025-12-13 23:16:38 +0500

    refactor: removed the common_handlers crutch.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from handlers.common import common_handlers
+from handlers.common import router as common_router
 from handlers.inline import router as inline_router
 
 bot = Bot(token=BOT_TOKEN)
@@ -10,7 +10,7 @@ dp = Dispatcher()
 
 
 async def main():
-    common_handlers(dp)
+    dp.include_router(common_router)
     dp.include_router(inline_router)
     
     await dp.start_polling(bot)
