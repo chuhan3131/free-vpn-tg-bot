09b8902159b2d111f99c9eb2747c790ce9af7ecf
chuhan <chuhanchik3131@gmail.com>
2025-12-19 06:54:10 +0500

    chore: update router configuration

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from handlers.common import router as common_router
+from handlers.user_handlers import router as user_router
 from handlers.inline import router as inline_router
 from handlers.callbacks import router as callbacks_router
+from handlers.admin_handlers import router as admin_router
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
 async def main():
+    dp.include_router(admin_router)
     dp.include_router(callbacks_router)
-    dp.include_router(common_router)
+    dp.include_router(user_router)
     dp.include_router(inline_router)
     
     await dp.start_polling(bot)
