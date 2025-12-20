90533ca50754a6fa024b8c4f5617056267aa2515
chuhan <chuhanchik3131@gmail.com>
2025-12-20 23:33:12 +0500

    refactor: changed the import logic

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from os import getenv
 
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = getenv("BOT_TOKEN") or ""
 DATA_FILE = getenv("DATA_FILE") or "data.json"
-ADMIN_ID = int(getenv("ADMIN_ID")) or 8402669285
+ADMIN_ID = int(getenv("ADMIN_ID")) 
+CHANNEL_ID = getenv("CHANNEL_ID")
 PUBLIC_KEY = "b7a92b4cd1a2ced29e06059c61f624be"
+
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN environment variable is required")
